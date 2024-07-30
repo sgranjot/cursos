d59d4eecbfa83814fb3d049c8f7f2f5bd580cf5f
Sergio Granjo <sgranjot@gmail.com>
2024-07-30 18:01:29 +0200

    CentroFormacionServiceImpl modified

---
+++ src/test/java/com/granjo/cursos/service/CentroFormacionServiceTest.java
@@ -0,0 +1,12 @@
+package com.granjo.cursos.service;
+
+import org.mockito.InjectMocks;
+
+public class CentroFormacionServiceTest {
+
+    @InjectMocks
+    CentroFormacionServiceImpl centroFormacionService;
+
+    
+
+}
